6f29c46267c783dabcc91af1013c597505f1fa06
Cortney Quinn <143303365+Sweetipi02@users.noreply.github.com>
2023-10-09 00:48:14 +0000

    excuses

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-window.onload = function getExcuse() {
+
+window.onload = function theExcuse() {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let done = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
@@ -25,10 +25,9 @@ window.onload = function getExcuse() {
     setElement(what) +
     " " +
     setElement(when);
-  //this will print your line of excuses
-  console.log(generateExcuses);
-  //this will put the excuse in the highlighted area.
-  const myDOMExcuse = (document.getElementByTagName("p") /
-  myDOMExcuse[0].innerText = generateExcuses);
-  console.log(myDOMExcuse[0]);
+
+  
+  const myDONExcuse = (document.getElementByTagName("p") 
+  myDONExcuse[0].innerText = generateExcuses);
+  console.log(myDONExcuse[0]);
 };
